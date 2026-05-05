5a6d7c20ce7c6d0ebbd437549020b15736089a25
ADARSH DUBEY <139523805+adarshiiit0117@users.noreply.github.com>
2026-05-05 11:26:46 +0530

    Refactor retrieve_context function in rag_service.py
    Refactor retrieve_context function to simplify knowledge retrieval and remove unused variables.

--- backend/app/services/rag_service.py
+++ backend/app/services/rag_service.py
@@ -1,35 +1,25 @@
-from sentence_transformers import SentenceTransformer
-import faiss
-import pickle
-import numpy as np
-import os
+def retrieve_context(user_query: str):
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    with open("app/data/gold_knowledge.txt", "r", encoding="utf-8") as file:
 
-INDEX_PATH = os.path.join(BASE_DIR, "vector_store", "gold_index.faiss")
-DOC_PATH = os.path.join(BASE_DIR, "vector_store", "documents.pkl")
+        knowledge = file.readlines()
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+    relevant_chunks = []
 
-index = faiss.read_index(INDEX_PATH)
+    query_words = user_query.lower().split()
 
-with open(DOC_PATH, "rb") as f:
-    documents = pickle.load(f)
+    for line in knowledge:
 
+        for word in query_words:
 
-def retrieve_context(user_query: str, top_k: int = 3):
+            if word in line.lower():
 
-    query_embedding = model.encode([user_query])
+                relevant_chunks.append(line.strip())
 
-    query_embedding = np.array(query_embedding).astype("float32")
+                break
 
-    distances, indices = index.search(query_embedding, top_k)
+    if not relevant_chunks:
 
-    retrieved_docs = []
+        relevant_chunks = knowledge[:3]
 
-    for idx in indices[0]:
-
-        if idx < len(documents):
-            retrieved_docs.append(documents[idx])
-
-    return "\n".join(retrieved_docs)
+    return "\n".join(relevant_chunks)
