0c8ff004432948d4df9a6474dc8fff9712b109aa
ADARSH DUBEY <139523805+adarshiiit0117@users.noreply.github.com>
2026-05-05 10:43:22 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://kuber-ai-wheat.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
